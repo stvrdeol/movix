7adb84857993e9c4259eb2cbe0d70593923b5ddb
Satvir Singh <stvrdeol@gmail.com>
2023-11-14 19:53:27 +0530

    fix some css in carousel section

--- src/components/CarouselComponent.jsx
+++ src/components/CarouselComponent.jsx
@@ -1,10 +1,7 @@
 import { CircularProgressbar, buildStyles } from "react-circular-progressbar";
 import "react-circular-progressbar/dist/styles.css";
 import { AiOutlineArrowLeft, AiOutlineArrowRight } from "react-icons/ai";
-import {
-  LazyLoadComponent,
-  LazyLoadImage,
-} from "react-lazy-load-image-component";
+import { LazyLoadImage } from "react-lazy-load-image-component";
 import "react-lazy-load-image-component/src/effects/blur.css";
 import { useSelector } from "react-redux";
 import { Link } from "react-router-dom";
@@ -49,7 +46,7 @@ function CarouselComponent({ data, loading, id }) {
     <section
       className="overflow-auto sm:overflow-hidden  w-full scroll-smooth snap-mandatory "
       id={id}>
-      <section className="flex h-max gap-2 sm:gap-4 max-w-none mt-6 w-max snap-mandatory snap-start relative">
+      <section className="flex items-stretch gap-2 sm:gap-4 max-w-none mt-6 w-max  relative">
         {loading ? (
           <>
             <section>
@@ -120,58 +117,54 @@ function CarouselComponent({ data, loading, id }) {
                     : `/details/movie/${result.id}`
                 }>
                 <article className="sm:max-w-[17vw] md:w w-[35vw] snap-start">
-                  <LazyLoadComponent>
-                    <section className="relative w-full h-max">
-                      <section>
-                        <LazyLoadImage
-                          threshold={250}
-                          src={`https://image.tmdb.org/t/p/original${result.poster_path}`}
-                          effect="blur"
-                          className="rounded-lg relative h-full max-w-full"
-                          wrapperProps={{
-                            style: { transitionDelay: ".1s" },
-                          }}
-                        />
-                        <section className="hidden absolute text-xs md:flex gap-2 bottom-2 right-1 w-1/2 justify-end flex-wrap">
-                          {genres[0] && (
-                            <p className=" p-1 w-max bg-pink rounded-md">
-                              {genres[0]}
-                            </p>
-                          )}
-                          {genres[0] !== genres[1] && genres[1] && (
-                            <p className="p-1 w-max rounded-md  bg-pink">
-                              {genres[1]}
-                            </p>
-                          )}
-                        </section>
-                      </section>
-
-                      <section className="w-12  absolute -bottom-6 left-3">
-                        <CircularProgressbar
-                          value={result.vote_average.toFixed(1)}
-                          maxValue={10}
-                          text={result.vote_average.toFixed(1)}
-                          styles={buildStyles({
-                            textSize: "40px",
-                            fontStyle: "bold",
-                            pathColor: `${pathColor(result.vote_average)}`,
-                            textColor: "black",
-                            trailColor: "#d6d6d6",
-                            backgroundColor: "#3e98c7",
-                          })}
-                          className="bg-white rounded-full p-0.5"
-                        />
+                  <section className="relative w-full h-max">
+                    <section>
+                      <LazyLoadImage
+                        threshold={250}
+                        src={`https://image.tmdb.org/t/p/original${result.poster_path}`}
+                        effect="blur"
+                        className="rounded-lg relative h-full max-w-full"
+                        wrapperProps={{
+                          style: { transitionDelay: ".1s" },
+                        }}
+                      />
+                      <section className="hidden absolute text-xs md:flex gap-2 bottom-2 right-1 w-1/2 justify-end flex-wrap">
+                        {genres[0] && (
+                          <p className=" p-1 w-max bg-pink rounded-md">
+                            {genres[0]}
+                          </p>
+                        )}
+                        {genres[0] !== genres[1] && genres[1] && (
+                          <p className="p-1 w-max rounded-md  bg-pink">
+                            {genres[1]}
+                          </p>
+                        )}
                       </section>
                     </section>
-                    <p className="mt-10 font-medium sm:text-lg md:text-xl  truncate">
-                      {result.title || result.name || "No Title"}
-                    </p>
-                    <p className="mt-1 text-[#7B8490]">
-                      {result.release_date ||
-                        result.first_air_date ||
-                        "No info"}
-                    </p>
-                  </LazyLoadComponent>
+
+                    <section className="w-12  absolute -bottom-6 left-3">
+                      <CircularProgressbar
+                        value={result.vote_average.toFixed(1)}
+                        maxValue={10}
+                        text={result.vote_average.toFixed(1)}
+                        styles={buildStyles({
+                          textSize: "40px",
+                          fontStyle: "bold",
+                          pathColor: `${pathColor(result.vote_average)}`,
+                          textColor: "black",
+                          trailColor: "#d6d6d6",
+                          backgroundColor: "#3e98c7",
+                        })}
+                        className="bg-white rounded-full p-0.5"
+                      />
+                    </section>
+                  </section>
+                  <p className="mt-10 font-medium sm:text-lg md:text-xl  truncate">
+                    {result.title || result.name || "No Title"}
+                  </p>
+                  <p className="mt-1 text-[#7B8490]">
+                    {result.release_date || result.first_air_date || "No info"}
+                  </p>
                 </article>
               </Link>
             );
